dbdbd101d31863db12b78796c8a65766b8caf5ef
papiando <info@papiando.com>
2019-03-20 21:54:25 -0400

    Implement new autoload

--- .bootstrap.php
+++ .bootstrap.php
@@ -6,6 +6,11 @@
   defined('__BASE__') || define('__BASE__',sprintf("%s://%s",isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',$_SERVER['HTTP_HOST']));
   defined('__VERSION') || define('__VERSION__','0.0.1');
 
-  // Call composer autoload
-  require(__ROOT__.DS.'vendor'.DS.'autoload.php');
+
+  // Auto-register classes
+  spl_autoload_register(function($class) {
+    // Use namespace as path
+    if(file_exists(__ROOT__.DS.str_replace('\\', DS, $class).'.php'))
+      require_once __ROOT__.DS.str_replace('\\', DS, $class).'.php';
+  });
 ?>
